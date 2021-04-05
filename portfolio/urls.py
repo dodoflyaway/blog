6a577b7c6fed391178818bac4b764e10103d1e0c
dodoflyawaydodoflyaway <munisc18@gmail.com>
2021-04-05 16:31:30 +0530

    m

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import myfunc
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',admin.site.urls),
+    path('',myfunc.myadd,name='home'),
+
 ]
