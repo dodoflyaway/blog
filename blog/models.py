aed1db627c6791c155192586ca492b475e56a596
dodoflyawaydodoflyaway <munisc18@gmail.com>
2021-04-12 10:48:06 +0530

    added bootstarp

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- jobs/admin.py
+++ jobs/admin.py
@@ -3,5 +3,4 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Job
-
 admin.site.register(Job)
